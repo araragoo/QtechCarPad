5390ff23fe1e486c7088746cbf0f3f1ebbf39d40
araragoo <arara_goo@yahoo.co.jp>
2019-05-04 02:29:18 +0900

    add new file

--- Motor.ts
+++ Motor.ts
@@ -20,6 +20,26 @@ namespace モータ {
     const DRV_MAX_B  37 //  6-37lsb : 0.48-5.06V   3Vmax -> (3.0-0.48)/(5.06-0.48)*(63-6)+6 = 37lsb
     const DRV_MAXMAX_B      0x3F
 
+    // サーボ
+    const PCA9685_ADDRESS = 0x40
+    const MODE1 = 0x00
+    const PRESCALE = 0xFE
+    const LED0_ON_L = 0x06
+
+    const PWM_FREQUENCY 50        //50Hz 20ms
+    const PWM_MAX 2400*4096/20000 //2.4ms
+    const PWM_MIN  500*4096/20000 //0.5ms
+    const PWM_MAX_B    4095       //4095lsb
+    const PWM_MIN_B    0          //   0lsb
+
+    const DEGREE_MIN  -90 //-90deg.
+    const DEGREE_MAX   90 // 90deg.
+
+    let initialized = false
+
+
+    const LED_MIN   0 //  0V
+    const LED_MAX 100 //3.3V
 
     function i2cwrite(addr: number, reg: number, value: number): void {
         let buf = pins.createBuffer(2)
@@ -43,7 +63,6 @@ namespace モータ {
         }
 
         let ctr;
-        let value;
         if (voltage == 0) {
             ctr = M_STANBY;
         } else if (voltage > 0) {
@@ -53,20 +72,88 @@ namespace モータ {
             voltage = -voltage;
         }
 
-        if(voltage > DRV_MAX) value = DRV_MAX;
-        if(voltage < DRV_MIN) value = DRV_MIN;
-        value = (DRV_MAX_B - DRV_MIN_B)*(value - DRV_MIN) / (DRV_MAX - DRV_MIN) + DRV_MIN_B;
-        value = ctr + (value << 2);
+        let val = voltage;
+        if(val > DRV_MAX) val = DRV_MAX;
+        if(val < DRV_MIN) val = DRV_MIN;
+        
+        val = (DRV_MAX_B - DRV_MIN_B)*(val - DRV_MIN) / (DRV_MAX - DRV_MIN) + DRV_MIN_B;
+        val = ctr + (val << 2);
 
-        i2cwrite(channel,CTR_ADR, value);
+        i2cwrite(channel,CTR_ADR, val);
     }
 
     // blockId=setMotor block="drive channel|%channel|voltage %voltage"
-    //% blockId=setMotor block="回転 右／左|%channel|電圧 %voltage"
+    //% blockId=setMotor block="モータ 右(0)/左(1)|%channel|電圧(-100～100) %voltage"
     //% weight=85
     //% channel.min=0 channel.max=1
     //% voltage.min=-100 voltage.max=100
     export function drive(channel: number,voltage: number): void {
         driveMotor(channel, voltage);
+    }
+
+    function initPCA9685(): void {
+        i2cwrite(PCA9685_ADDRESS, MODE1, 0x00)
+        setFreq(50);
+        setPwm(0, 0, 4095);
+        for (let idx = 1; idx < 16; idx++) {
+            setPwm(idx, 0, 0);
+        }
+        initialized = true
+    }
+
+    function setFreq(freq: number): void {
+        // Constrain the frequency
+        freq *= 0.9;  // Correct for overshoot in the frequency setting (see issue #11).
+        let prescaleval = 25000000;
+        prescaleval /= 4096;
+        prescaleval /= freq;
+        prescaleval -= 1;
+        let prescale = prescaleval; //Math.Floor(prescaleval + 0.5);
+        let oldmode = i2cread(PCA9685_ADDRESS, MODE1);
+        let newmode = (oldmode & 0x7F) | 0x10; // sleep
+        i2cwrite(PCA9685_ADDRESS, MODE1, newmode); // go to sleep
+        i2cwrite(PCA9685_ADDRESS, PRESCALE, prescale); // set the prescaler
+        i2cwrite(PCA9685_ADDRESS, MODE1, oldmode);
+        control.waitMicros(5000);
+        i2cwrite(PCA9685_ADDRESS, MODE1, oldmode | 0xa1);
+    }
+
+    function setPwm(channel: number, on: number, off: number): void {
+        if (channel < 0 || channel > 15)
+            return;
+
+        let buf = pins.createBuffer(5);
+        buf[0] = LED0_ON_L + 4 * channel;
+        buf[1] = on & 0xff;
+        buf[2] = (on >> 8) & 0xff;
+        buf[3] = off & 0xff;
+        buf[4] = (off >> 8) & 0xff;
+        pins.i2cWriteBuffer(PCA9685_ADDRESS, buf);
+    }
+
+    //% blockId=setServo block="サーボ 右靴(0)/左靴(1)/右足(2)/左足(3)|%channel|角度 %degree"
+    //% weight=85
+    //% channel.min=0 channel.max=3
+    //% degree.min=-45 degree.max=45
+    export function Servo(channel: number,degree: number): void {
+        if (!initialized) {
+            initPCA9685();
+        }
+        let val = degree;
+        val = (val-DEGREE_MIN) * (PWM_MAX-PWM_MIN) / (DEGREE_MAX-DEGREE_MIN);
+        setPWM(channel+4, 0, val);
+    }
+
+    //% blockId=setLED block="LED 赤(0)/黄(1)/緑(2)/青(3)|%channel|電圧(-100～100) %voltage"
+    //% weight=85
+    //% channel.min=0 channel.max=3
+    //% voltage.min=0 voltage.max=100
+    export function LED(channel: number,voltage: number): void {
+        if (!initialized) {
+            initPCA9685();
+        }
+        let val = voltage;
+        val = (val-LED_MIN) * (PWM_MAX-PWM_MIN) / (LED_MAX-LED_MIN);
+        setPwm(channel, 0, val);
     }
 } 
