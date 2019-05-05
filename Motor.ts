1e4a3e4381c34f5364d21272af85634d3dc75680
araragoo <arara_goo@yahoo.co.jp>
2019-05-05 17:01:40 +0900

    add new file

--- Motor.ts
+++ Motor.ts
@@ -39,10 +39,10 @@ namespace Motor {
     let initialized = false
 
     function i2cwrite(addr: number, reg: number, value: number) {
-        let buf = pins.createBuffer(2)
-        buf[0] = reg
-        buf[1] = value
-        pins.i2cWriteBuffer(addr, buf)
+        let buf = pins.createBuffer(2);
+        buf[0] = reg;
+        buf[1] = value;
+        pins.i2cWriteBuffer(addr, buf);
     }
 
     function i2cread(addr: number, reg: number) {
