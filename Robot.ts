5d36b0d1ead83085be35dc4a788d459882e13bf5
Hitoshi Arakawa <arakawa@tech-i.kyutech.ac.jp>
2019-06-01 01:10:46 +0900

    Update Robot.ts

--- Robot.ts
+++ Robot.ts
@@ -141,8 +141,7 @@ namespace Robot {
         if (!initialized) {
             initPCA9685();
         }
-        degree += 150;
-        let v_us = (degree * 95 / 9 + 500); // 0.5 ~ 2.4 ms
+        let v_us = (degree * 95 / 9 + 500 + 1900/2); // 0.5 ~ 2.4 ms
 //        let v_us = ((degree+100) * 1900 / 180 + 500); // 0.5 ~ 2.4 ms
 //        let v_us = degree; // 0.5 ~ 2.4 ms
         let val = v_us * 4096 / 20000; // 50hz: 20,000 us
