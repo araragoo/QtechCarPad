1e17be903c6eda8c701a07e15179b6f30c7b930b
Hitoshi Arakawa <arakawa@tech-i.kyutech.ac.jp>
2019-06-08 00:24:55 +0900

    Update Robot.ts

--- Robot.ts
+++ Robot.ts
@@ -134,7 +134,7 @@ namespace Robot {
 
     //  subcategory="Robot"
     //  blockId=setServo block="Servo LowR:0 LowL:1 HighR:2 HighL:3 %channel|degree:-45Å`45 %degree"
-    //% blockId=setServo block="Servo LowR:0 LowL:1 HighR:2 HighL:3 %channel|degree:0Å`180 %degree"
+    //% blockId=setServo block="Servo FrontR:0 FrontL:1 RearR:2 RearL:3 %channel|degree:0Å`180 %degree"
     //% weight=85
     //% channel.min=0 channel.max=3
     //% degree.min=0 degree.max=180
@@ -142,7 +142,7 @@ namespace Robot {
         if (!initialized) {
             initPCA9685();
         }
-        let v_us = (degree * 95 / 9 + 500); // 0.5 ~ 2.4 ms <=> offset 120dge.
+        let v_us = (degree * 95 / 9 + 500); // 0.5 ~ 2.4 ms <=> offset 0dge.
 //        let v_us = (degree * 95 / 9 + 500 + 1900*120/180); // 0.5 ~ 2.4 ms <=> offset 120dge.
 //        let v_us = ((degree+100) * 1900 / 180 + 500); // 0.5 ~ 2.4 ms
         let val = v_us * 4096 / 20000; // 50hz: 20,000 us
