24b5822d8591615d960942a1a8e1fa8b4ce31a59
Hitoshi Arakawa <arakawa@tech-i.kyutech.ac.jp>
2019-06-14 23:47:46 +0900

    Update Insect.ts

--- Insect.ts
+++ Insect.ts
@@ -2,6 +2,9 @@
 
 //% weight=5 color=#0fbc11 icon="\uf112" block="Insect"
 namespace Insect {
+    let prev_y = 0
+    let prev_x = 0
+
 
     //  subcategory="Insect"
     //% blockId=prgInsect block="prg x:0<=>4 %x|y:0<=>4 %y"
@@ -9,6 +12,12 @@ namespace Insect {
     //% x.min=0 x.max=4
     //% y.min=0 y.max=4
     export function prg(x: number,y: number): void {
-        radio.sendString("m" + x + y);
+        if (x != prev_x || y != prev_y) {
+            led.unplot(prev_x, prev_y)
+        }
+        led.plot(x, y)
+        prev_x = x
+        prev_y = y
+        radio.sendString("m" + x + y)
     }
 } 
