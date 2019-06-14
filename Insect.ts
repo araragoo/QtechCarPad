76c7d0c1932fb870d5a956e3d8e8ab538b72747a
Hitoshi Arakawa <arakawa@tech-i.kyutech.ac.jp>
2019-06-15 00:08:43 +0900

    Update Insect.ts

--- Insect.ts
+++ Insect.ts
@@ -5,13 +5,7 @@ namespace Insect {
     let prev_y = 0
     let prev_x = 0
 
-
-    //  subcategory="Insect"
-    //% blockId=prgInsect block="prg x:0<=>4 %x|y:0<=>4 %y"
-    //% weight=85
-    //% x.min=0 x.max=4
-    //% y.min=0 y.max=4
-    export function prg(x: number,y: number): void {
+    function drive(x: number, y: number): void {
         if (x != prev_x || y != prev_y) {
             led.unplot(prev_x, prev_y)
         }
@@ -20,4 +14,44 @@ namespace Insect {
         prev_y = y
         radio.sendString("m" + x + y)
     }
+
+    //  subcategory="Insect"
+    //% blockId=prgInsect block="prg x:0<=>4 %x|y:0<=>4 %y"
+    //% weight=85
+    //% x.min=0 x.max=4
+    //% y.min=0 y.max=4
+    export function prg(x: number,y: number): void {
+        drive(x, y)
+    }
+
+    //  subcategory="Insect"
+    //% blockId=prgFwd block="fwd"
+    //% weight=85
+    //% x.min=0 x.max=4
+    //% y.min=0 y.max=4
+    export function fwd(): void {
+        prg(2, 0)
+    }
+
+    //  subcategory="Insect"
+    //% blockId=prgBwd block="bwd"
+    //% weight=85
+    export function bwd(): void {
+        prg(2, 4)
+    }
+
+    //  subcategory="Insect"
+    //% blockId=prgRight block="right"
+    //% weight=85
+    export function right(): void {
+        prg(4, 2)
+    }
+
+    //  subcategory="Insect"
+    //% blockId=prgLeft block="left"
+    //% weight=85
+    export function left(): void {
+        prg(0, 2)
+    }
+
 } 
