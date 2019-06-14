c9af4bfc41cf19c0f4cc62d6ded2ca85067325b3
Hitoshi Arakawa <arakawa@tech-i.kyutech.ac.jp>
2019-06-14 23:26:23 +0900

    Update Insect.ts

--- Insect.ts
+++ Insect.ts
@@ -150,7 +150,7 @@ namespace Insect {
     }
 
     //  subcategory="Insect"
-    //% blockId=prgInsect block="x:0<=>4 %x|y:0<=>4 %y"
+    //% blockId=prgInsect block="prg x:0<=>4 %x|y:0<=>4 %y"
     //% weight=85
     //% x.min=0 x.max=4
     //% y.min=0 y.max=4
