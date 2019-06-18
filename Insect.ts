8effbb077f1747f0b23238a10040daf3a296180a
Hitoshi Arakawa <arakawa@tech-i.kyutech.ac.jp>
2019-06-18 14:26:23 +0900

    Update Insect.ts

--- Insect.ts
+++ Insect.ts
@@ -254,19 +254,5 @@ namespace Insect {
         return p2Count
     }
 
-    //% subcategory="Music"
-    //% blockId=prgInsect block="music1 No.0<=>9 %n"
-    //% weight=85
-    //% n.min=0 n.max=9
-    export function music1(n: number): void {
-        radio.sendString("s" + n)
-    }
 
-    //% subcategory="Music"
-    //% blockId=prgInsect block="music2 No.0<=>9 %n"
-    //% weight=85
-    //% n.min=0 n.max=9
-    export function music2(n: number): void {
-        radio.sendString("p" + n)
-    }
 } 
