7cc6b58070d13439327ce83cb118d483a2ffe99a
araragoo <arara_goo@yahoo.co.jp>
2019-11-04 14:10:35 +0900

    Update Cag.ts

--- Cag.ts
+++ Cag.ts
@@ -33,7 +33,7 @@ namespace Cag {
                 p1Count += 1
             } else if ("p2".compare(receivedString.substr(0, 2)) == 0) {
                 p2Count += 1
-            } else if ("d".compare(receivedString.charAt(0)) == 0) {
+            } else if ("x".compare(receivedString.charAt(0)) == 0) {
                 distanceData = parseFloat(receivedString.substr(1, receivedString.length-1))
             }
         }
